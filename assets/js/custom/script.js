f5f6c74cf5a628dfeed7d69ecc6d16bb448c58e4
MauricioModelit <mauricio.machado@modelit.xyz>
2017-05-30 23:14:29 -0300

    Updated login/registration forms

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -1,5 +1,43 @@
 /**
  * Custom Javascript (non-Angular) goes here
- * 
  * @since   Feb, 2017
  **/
+
+/**
+ * Handle the behaviour of the forms
+ */
+$(window, document, undefined).ready(function() {
+
+    $('input').blur(function() {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    var $ripples = $('.ripples');
+
+    $ripples.on('click.Ripples', function(e) {
+
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+        $(this).removeClass('is-active');
+    });
+
+});
